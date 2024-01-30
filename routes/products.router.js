c0a0274acdc7553770dd010bed9deb7f01867ebd
Donovan Rojas <rojasdrm03@gmail.com>
2024-01-30 10:10:14 -0500

    router version and created post method

--- routes/products.router.js
+++ routes/products.router.js
@@ -41,6 +41,14 @@ router.get('/:id', (req, res) => {
   )
 });
 
-
+router.post('/', (req, res) => {
+  const body= req.body;
+  res.json(
+    {
+      message: 'created',
+      data: body,
+    },
+  )
+});
 
 module.exports = router;
