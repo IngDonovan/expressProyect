276fbffb9b90b44913da8d87a535cd41aa292058
Donovan Rojas <rojasdrm03@gmail.com>
2024-01-30 15:20:31 -0500

    set status code

--- routes/products.router.js
+++ routes/products.router.js
@@ -29,21 +29,28 @@ router.get('/filter', (req, res) => {
 
 router.get('/:id', (req, res) => {
   const { id }= req.params;
-  res.json(
-    {
-      id,
-      title: 'Product 1',
-      price: 1000,
-      category:'jewlery',
-      description:'lalala',
-      image:'url.....'
-    },
-  )
+  if (id === '999') {
+    res.status(404).json(
+      {
+        message: 'not found',
+      },
+    )
+  } else {
+    res.status(200).json(
+      {
+        id,
+        title: 'Product 1',
+        price: 1000,
+        category:'jewlery',
+        description:'lalala',
+        image:'url.....'
+      },
+  )}
 });
 
 router.post('/', (req, res) => {
   const body= req.body;
-  res.json(
+  res.status(201).json(
     {
       message: 'created',
       data: body,
