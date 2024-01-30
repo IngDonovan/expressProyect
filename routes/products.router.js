883a97e498f952c58d54122e5141cb9aa4183740
Donovan Rojas <rojasdrm03@gmail.com>
2024-01-30 17:28:20 -0500

    create Class to services

--- routes/products.router.js
+++ routes/products.router.js
@@ -1,23 +1,12 @@
 const express = require('express');
-const { faker } = require('@faker-js/faker');
+
+const ProductsService = require('../services/product.service')
 
 const router = express.Router();
+const service = new ProductsService();
 
 router.get('/', (req, res) => {
-  const { size } = req.query;
-  const products = [];
-  const limit = size || 10;
-  for (let index = 0; index < limit; index++) {
-    products.push({
-      id: faker.string.uuid(),
-      title: faker.commerce.productName(),
-      price: parseInt(faker.commerce.price(), 10),
-      category: faker.commerce.department(),
-      description: faker.commerce.productDescription(),
-      image: faker.image.url(),
-    });
-
-  }
+  const products = service.find();
   res.json(products)
 });
 
@@ -29,23 +18,8 @@ router.get('/filter', (req, res) => {
 
 router.get('/:id', (req, res) => {
   const { id }= req.params;
-  if (id === '999') {
-    res.status(404).json(
-      {
-        message: 'not found',
-      },
-    )
-  } else {
-    res.status(200).json(
-      {
-        id,
-        title: 'Product 1',
-        price: 1000,
-        category:'jewlery',
-        description:'lalala',
-        image:'url.....'
-      },
-  )}
+  const product = service.findOne(id);
+  res.json(product)
 });
 
 router.post('/', (req, res) => {
