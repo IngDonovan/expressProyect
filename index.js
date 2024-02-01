2a28c84f4817b669f231e2c8fc8489fb249e2489
Donovan Rojas <rojasdrm03@gmail.com>
2024-02-01 09:04:45 -0500

    for deploy change the por to env.port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const routerApi = require('./routes');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const { logErrors,  errorHandler, boomErrorHandler } = require('./middlewares/error.handler');
 
